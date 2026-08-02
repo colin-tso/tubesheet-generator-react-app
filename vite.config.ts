362cb35c3c2864631d9b3f73f5dbdb9c5dbb81ff
GrandTheftGelato <colintso@hotmail.com>
2026-08-02 15:38:27 +1000

    implement https local testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
     base: "/tubesheet-generator-react-app/",
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), mkcert()],
     build: {
         outDir: "build",
     },
